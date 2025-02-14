23f5ba6d2881056e140438eefdb2d41f4d6d2683
Cristian Somma <12999453@itisrossi.vi.it>
2025-02-14 08:42:23 +0100

    ottimizzazione compare loop

--- if-else.asm
+++ if-else.asm
@@ -37,22 +37,20 @@
         
         ;-CICLO FOR-             
         MOV CX, 2h  ; Assegna al registro contatore il numero di cicli da effettuare, ovvero due
-        LEA SI, num1    ; muovo in SI l'indirizzo di memoria del num1                
-        ;LEA DI, num2    ; LINEA CHE DOVREBBE ESSERCI MA NON SEMBRA NECESSARIA ???
-        compare_loop:   ; Label che segna l'inizio del ciclo for
+        MOV SI, 0000h   ; definisco SI, ovvero l'offset a zero per scorrere nel vettore dei due numeri
+ compare_loop:   ; Label che segna l'inizio del ciclo for
         CMP CX, 2h  ; se il contatore (CX) è 2...
         JE cx_equ   ; ... salta alla label cx_equ
         MOV AL, num1[SI]    ; altrimenti salva in al il valore all'indirizzo SI di num1
-        MOV BL, num2[DI]    ; salva in bl il valore all'indirizzo DI di num2
+        MOV BL, num2[SI]    ; salva in bl il valore all'indirizzo DI di num2
         JMP end_compare_loop    ; salta incondizionatamente alla label end_compare_loop
         
-        cx_equ: ; Label che indica il blocco di codice dell'if true
+ cx_equ: ; Label che indica il blocco di codice dell'if true
         MOV AH, num1[SI]    ; muove in ah il valore all'indirizzo SI di num1    
-        MOV BH, num2[DI]    ; stessa cosa viene fatta per num2 in bh
+        MOV BH, num2[SI]    ; stessa cosa viene fatta per num2 in bh
            
-        end_compare_loop:   ; label che indica la parte del ciclo comune ad entrambi i blocchi
-        INC SI  ; incremento di 1 il registro si per puntare all'indirizzo subito seguente
-        INC DI  ; faccio lo stesso anche con di
+ end_compare_loop:   ; label che indica la parte del ciclo comune ad entrambi i blocchi
+        INC SI  ; incremento di 1 il registro si per puntare all'indirizzo subito seguente  
         LOOP compare_loop   ; se cx != 0 allora decrementa di uno e torna all'inizio del ciclo for
         
         ;-COMPARAZIONI-
@@ -64,15 +62,15 @@
         JGE exe_subtraction ; ...è maggiore o uguale a bl allora chiama la sottrazione...
         JL exe_addition    ; ...se invece è minore di bl chiama l'addizione
         
-        exe_subtraction:    ; blocco condizionale chiamato quando bisogna eseguire la sottrazione
+ exe_subtraction:    ; blocco condizionale chiamato quando bisogna eseguire la sottrazione
         CALL subtraction    ; chiama la subroutine di sottrazione
         JMP end_program ; salta incondizionalmente al blocco end_program
         
-        exe_addition:   ; blocco condizionale chiamato quando bisogna eseguire la somma
+ exe_addition:   ; blocco condizionale chiamato quando bisogna eseguire la somma
         CALL addition   ; chiama la subroutine di somma
         
         ;-PARTE CONCLUSIVA-
-        end_program:    ; blocco di codice comune
+ end_program:    ; blocco di codice comune
         ; per evitare sovrascrizione involontarie:
         MOV BH, AH  ; bh assume il valore di ah
         MOV BL, AL  ; bl assume il valore di al
@@ -94,16 +92,16 @@
     ends
     ;---------------------------------------------------------------------------------------------          
     input PROC NEAR
-        MOV CX, 2   ; assegno al counter 2, ovvero il numero di input da ricevere
+            MOV CX, 2   ; assegno al counter 2, ovvero il numero di input da ricevere
         input_loop:  ; Label che indica l'inizio del loop
-        MOV AH, 01h ; Utilizzo la funzione di input con echo
-        int 21h ; Richiamo l'interrupt
-        SUB AL, 30h ; Sottraggo 48 (30h) al codice ASCII inserito per ottenere il valore numerico
-        MOV [SI], AL   ; Inserisco in num1, all'indirizzo determinato tramite SI l'input
-        INC SI  ; Incremento di uno il registro SI (Source Index)
-        LOOP input_loop:   ; se cx != 0 salta alla label input_loop e riesegue il codice
-        CALL return ; chiamo la subroutine per tornare a capo riga
-        RET ; Ritorna alla routine Main
+            MOV AH, 01h ; Utilizzo la funzione di input con echo
+            int 21h ; Richiamo l'interrupt
+            SUB AL, 30h ; Sottraggo 48 (30h) al codice ASCII inserito per ottenere il valore numerico
+            MOV [SI], AL   ; Inserisco in num1, all'indirizzo determinato tramite SI l'input
+            INC SI  ; Incremento di uno il registro SI (Source Index)
+            LOOP input_loop:   ; se cx != 0 salta alla label input_loop e riesegue il codice
+            CALL return ; chiamo la subroutine per tornare a capo riga
+            RET ; Ritorna alla routine Main
     input ENDP    ; Fine della subroutine
     ;---------------------------------------------------------------------------------------------   
     print PROC NEAR
@@ -125,31 +123,31 @@
     return ENDP ; Fine della subroutine
     ;---------------------------------------------------------------------------------------------
     addition PROC NEAR  ; subroutine per eseguire l'addizione dei due numeri
-        ADD AL, BL  ; somma le due cifre meno significative dei due numeri
-        CMP AL, 0Ah ; se il risultato è maggiore di 10...
-        JGE carry   ; ...salta alla label carry che gestisce il riporto
-        ADD AH, BH  ; altrimenti somma le due cifre più significative
-        RET ; ritorna alla routine main
-        
+            ADD AL, BL  ; somma le due cifre meno significative dei due numeri
+            CMP AL, 0Ah ; se il risultato è maggiore di 10...
+            JGE carry   ; ...salta alla label carry che gestisce il riporto
+            ADD AH, BH  ; altrimenti somma le due cifre più significative
+            RET ; ritorna alla routine main
+            
         carry:  ; blocco che viene chiamato nel momento in cui c'è un riporto
-        SUB AL, 0Ah ; sottrae 10 per ottenere un numero a cifra singola
-        ADD AH, BH  ; somma le due cifre più significative
-        ADD AH, 1   ; somma al risultato il riporto
-        RET
+            SUB AL, 0Ah ; sottrae 10 per ottenere un numero a cifra singola
+            ADD AH, BH  ; somma le due cifre più significative
+            ADD AH, 1   ; somma al risultato il riporto
+            RET
     addition ENDP   ; fine della subroutine
     ;---------------------------------------------------------------------------------------------
     subtraction PROC NEAR   ; subroutine che esegue la sottrazione
-        SUB AL, BL  ; sottrazione tra le due cifre meno significative
-        CMP AL, 0000h   ; compara il risultato a zero, se al...
-        JL back_carry   ; ...è minore di zero salta al blocco back_carry
-        SUB AH, BH  ; ...altrimenti esegue la sottrazione tra le due cifre più significative
-        RET ; ritorna alla routine main
-        
+            SUB AL, BL  ; sottrazione tra le due cifre meno significative
+            CMP AL, 0000h   ; compara il risultato a zero, se al...
+            JL back_carry   ; ...è minore di zero salta al blocco back_carry
+            SUB AH, BH  ; ...altrimenti esegue la sottrazione tra le due cifre più significative
+            RET ; ritorna alla routine main
+            
         back_carry: ; blocco eseguito se il AL < 0
-        ADD AL, 0Ah  ; somma ad al 10 per avere il valore positivo corretto
-        SUB AH, BH  ; sottrazione tra i due numeri più significativi
-        SUB AH, 1   ; sottrazione del riporto prestato alle cifre meno significative
-        RET
+            ADD AL, 0Ah  ; somma ad al 10 per avere il valore positivo corretto
+            SUB AH, BH  ; sottrazione tra i due numeri più significativi
+            SUB AH, 1   ; sottrazione del riporto prestato alle cifre meno significative
+            RET
     subtraction ENDP
     ;---------------------------------------------------------------------------------------------
     print_convert_char PROC NEAR
